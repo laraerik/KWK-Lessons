1bb41748b6ab0c27bb56cee14cb985e9e4fc8b5b
laraerik <32204494+laraerik@users.noreply.github.com>
2020-07-27 19:50:55 -0400

    Add files via upload

---
+++ ToDoList/ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by gwc on 7/27/20.
+//  Copyright © 2020 gwc. All rights reserved.
+//
+
+import UIKit
+
+class ToDoClass{
+    var description = ""
+    var important = false
+}
